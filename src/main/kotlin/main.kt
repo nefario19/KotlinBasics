0c677fbb4328418ccfe6020b0394f788fd4826a1
Taif Rahim <77005246+nefario19@users.noreply.github.com>
2021-02-06 13:20:08 +0100

    First version
    This is the first version of the challenges for the Kotlin tutorial.

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,87 @@
+fun main(args: Array<String>) {
+/*
+    Challenge 1: Take a monthly salary of 3404 euro. Convert this to a weekly salary.
+
+    Challenge hint: Use two variables and the print line function to show this in the console.
+ */
+    println("------Challenge 1-------------------------------")
+
+
+/*
+    Challenge 2: Try to figure out why the code under this comment is will not work
+*/
+    println("------Challenge 2-------------------------------")
+    val name: String = "Enter your name here"
+    println(name)
+
+    name = "Eric Cartman"
+    println(name)
+
+/*
+    Challenge 3: Create the following:
+    - Two variables and name them z and k. Give them the values 8 and 5.
+    - Multiply the two variables with each other.
+    - Divide the two by 3.5.
+    - Refactor the 'z' variable to 'car' and 'k' to horsepower.
+    - Print the values to the console.
+*/
+    println("------Challenge 3-------------------------------")
+
+
+/*
+    Challenge 4: Use string interpolation
+
+    Taif like to cook in the weekends. In the morning he usually uses 2 eggs.
+    After whisking the eggs he then uses 1 teaspoon sumac and 1.5 teaspoons salt.
+    Those are mixed with the eggs and then 3 teaspoons of black pepper are thrown
+    in the mix. Cooking that in an ordinary frying pan for 5 minutes results in a
+    great omelette.
+
+    - The text below has some information in them that can be extracted
+    and printed using string interpolation.
+    - Print three sentences using string interpolation and information in the
+    above anecdote.
+
+    Challenge hint: Search the internet for Kotlin string interpolation and
+    you'll find many examples. Also start making variables to store the information.
+ */
+
+    println("------Challenge 4-------------------------------")
+
+
+    /*
+
+    Challenge 5:
+
+    Copy and paste the variables in challenge 4 and paste them here.
+    Kotlin knows has something called type inference. Change the code
+    so that Kotlin uses this functionality.
+     */
+
+    println("------Challenge 5-------------------------------")
+
+    /*
+    Challenge 6:
+
+    Create a Kotlin switch case. Read the following anecdote.
+
+    When driving in an urban area, the maximum speed is 50 km/h.
+    When driving under 50 km/h that means you're driving slow.
+    When driving faster than 50 km/h you're driving too fast.
+
+    Write a switch case with three cases.
+    One case when you're driving exactly 50 prints a motivating line such as "great job!"
+    One case when you're driving to slow that prints something like "you should a little faster"
+    One case when you're driving to fast that prints a line that says that you should slow down.
+
+    You can make up your own lines.
+    */
+    println("------Challenge 6-------------------------------")
+
+    /*
+    Bonus round:
+
+    Take a look at challenge 6. Adjust the code that when you're driving too slow or too fast,
+    it prints how much faster or slower you have to go to reach 50 km/h.
+     */
+}
